1645ae5489de031792e0fc2d48b63046a304ea9d
HenriPucci <pucci.rique1234@gmail.com>
2023-04-14 12:50:50 -0300

    começando a fazer questionário

---
+++ public/js/questions.js
@@ -0,0 +1,35 @@
+export default [
+    {
+      question: "A respiração adequada deve ser realizada por qual(is) cavidade(s)?",
+      answers: [
+        { option: "Nariz", correct: true },
+        { option: "Boca", correct: false },
+        { option: "Nariz e Boca", correct: false },
+        { option: "Nenhuma das Alternativas", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 5 x 2?",
+      answers: [
+        { option: "9", correct: false },
+        { option: "10", correct: true },
+        { option: "11", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 10 x 8?",
+      answers: [
+        { option: "800", correct: false },
+        { option: "90", correct: false },
+        { option: "80", correct: true },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+      ],
+    },
+  ];
